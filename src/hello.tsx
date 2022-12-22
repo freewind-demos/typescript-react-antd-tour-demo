e84012d3d9318b859eeff677e18f07928eb492a3
Freewind <nowindlee@gmail.com>
2022-12-22 14:43:34 +0800

    typescript-react-antd-tour-demo

--- src/hello.tsx
+++ src/hello.tsx
@@ -1,6 +1,36 @@
-import React from 'react'
-import {Alert} from 'antd';
+import React, {useRef, useState} from 'react'
+import {Button, Space, Tour} from 'antd';
 
 export default function Hello() {
-  return <Alert message={'Hello'}/>
+    const [open, setOpen] = useState(false);
+    const ref1 = useRef(null)
+    const ref2 = useRef(null)
+    const ref3 = useRef(null)
+    return <Space direction={"vertical"}>
+        <div><Button onClick={() => setOpen(true)}>Tour</Button></div>
+        <Tour open={open} onClose={() => setOpen(false)} steps={[
+            {
+                title: '1111',
+                description: '11111',
+                target: () => ref1.current
+            },
+            {
+                title: '2222',
+                description: '22222',
+                target: () => ref2.current,
+            },
+            {
+                title: '3333',
+                description: '33333',
+                target: () => ref3.current,
+            }
+        ]}/>
+        <div>
+            <ul>
+                <li ref={ref1}>aaaa</li>
+                <li ref={ref2}>bbbb</li>
+                <li ref={ref3}>cccc</li>
+            </ul>
+        </div>
+    </Space>
 };
